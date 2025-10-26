57ab2badbea74aee6691ad5958074cb42c634d18
jim amuto <jim.amuto@strathmore.edu>
2025-10-26 21:22:34 +0300

    debugging

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-Hand');
-    const Hourhand = document.querySelector('.hour-Hand');
+const minHand = document.querySelector('.minute-hand');
+    const hourhand = document.querySelector('.hour-hand');
 
 function setDate(){
-const now =new setDate();
+const now =new Date();
 //seconds
 const seconds=now.getSeconds();
 const secondsDegrees=((seconds/60)*360)+90;
@@ -15,18 +15,18 @@ console.log(seconds);
 
 //minutes
 
-const minute=now.getMinutes();
-const minutesDegrees=((seconds/60)*360)+90;
-minutes.style.transform= `rotate(${minutesDegrees}deg)`;
+const minutes=now.getMinutes();
+const minutesDegrees=((minutes/60)*360)+90;
+minHand.style.transform= `rotate(${minutesDegrees}deg)`;
 
 
 
 
 
 //hours
-const hour=now.getHours();
-const hoursDegrees=((seconds/12)*360)+90;
-hourHand.style.transform= `rotate(${hoursDegrees}deg)`;
+const hours=now.getHours();
+const hoursDegrees=((hours/12)*360)+90;
+hourhand.style.transform= `rotate(${hoursDegrees}deg)`;
 
 
 
